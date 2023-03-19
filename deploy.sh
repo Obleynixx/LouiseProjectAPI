b000c62367c986070590183460f519704d4a3911
Gabriel Hampel Meireles <hampelgabriel@gmail.com>
2023-03-19 03:39:14 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --user
+python3 get-pip.py --user
 
 # Install requirements
-python -m pip install --user -r requirements.txt
+python3 -m pip install --user -r requirements.txt
